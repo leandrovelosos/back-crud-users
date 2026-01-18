ebeb9ecb0493c69f6ba0aa6390d14319ca2308da
leandrovelosos <leandrovdsantos96@gmail.com>
2026-01-18 09:58:44 -0300

    integracao-get

---
+++ db.js
@@ -0,0 +1,10 @@
+//config db
+
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "crud",
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+//configuracao do servidor
+
+import express from 'express';
+import userRoutes from './routes/users.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", userRoutes);
+
+app.listen(8800);
