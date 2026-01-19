7d561faa13586640b3eabb07e847f35f076dded2
leandrovelosos <leandrovdsantos96@gmail.com>
2026-01-18 22:12:51 -0300

    update routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 
 import express from 'express';
-import { getUsers } from  "../controllers/users.js";
+import { getUsers, addUser, updateUSer, deleteUser } from "../controllers/users.js";
 
+//rotas
 const router = express.Router();
 
 router.get("/", getUsers);
+
+router.post("/", addUser);
+
+router.put("/:id", updateUSer);
+
+router.delete("/:id", deleteUser);
 
 export default router;
