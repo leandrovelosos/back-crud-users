2aed228280d4e87c54c2cc9518019b5232846bfa
leandroiveloso <leandrovdsantos96@gmail.com>
2026-01-20 17:13:42 -0300

    update conection

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 //config db
 
-import mysql from "mysql";
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-export const db = mysql.createConnection({
+dotenv.config();
+
+export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
