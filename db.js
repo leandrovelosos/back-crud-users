a340b1de26e2bb6f5de6e5cac4e09a51e33339fb
leandroiveloso <leandrovdsantos96@gmail.com>
2026-01-20 11:48:02 -0300

    update-bck

--- db.js
+++ db.js
@@ -5,6 +5,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456",
+    password: "12345",
     database: "crud",
 });
